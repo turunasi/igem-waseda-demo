16d1a3fbe2d43b9194b9a6ac095be7460488eac6
Yuki Nishii <45918351+turunasi@users.noreply.github.com>
2020-03-31 20:21:57 +0900

    modify codes

--- temp/head.php
+++ temp/head.php
@@ -11,5 +11,5 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link href="css/index.css'" rel="stylesheet">
+<link href="css/index.css" rel="stylesheet">
 <title>iGEM-Waseda</title>
